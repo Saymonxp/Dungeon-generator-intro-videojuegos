fd647b5298f0446e5feb76e63f79327447667196
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2023-06-13 09:58:01 -0500

    Barra de vida y método de aumentar vida

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour, IDamageable
 {
     [field:SerializeField]
     public int TotalHealthPoints { get; private set; }
+    [field:SerializeField]
+    public Image LifeIndicator {get; private set; }
     public int HealthPoints { get; private set; }
     
     private void Start()
@@ -19,5 +22,18 @@ public class Player : MonoBehaviour, IDamageable
         HealthPoints--;
         if(HealthPoints <= 0)
             gameObject.SetActive(false);
+    }
+
+    public void AddLife()
+    {
+        if(HealthPoints + 1 > TotalHealthPoints)
+            return;
+    
+        HealthPoints++;
+    }
+
+    void Update()
+    {
+        LifeIndicator.fillAmount =  HealthPoints /  TotalHealthPoints;
     }
 }
